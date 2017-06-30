9748b69dedb0c02ee68ac3445c010cfab03a71d6
Sam Brewer <Sam Brewer>
2017-06-29 18:37:43 -0600

    toggle search

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,26 @@
 $(document).ready((e) => {
   $('#slide-out-nav').hide()
+  $('#search-screen').hide()
 
   $('#nav-button').on('click', () => {
     $('#slide-out-nav').show()
     $('body').css('overflow-y', 'hidden')
-  })
+  });
+
+  $('#search-button').click(() => {
+    $('#search-screen').show()
+  });
 
   $('#slide-out-nav').on('click', () => {
     $('#slide-out-nav').hide()
     $('body').css('overflow-y', 'auto')
-  })
+  });
+
   $('.slide-out-buttons').click((event) => {
     event.stopPropagation();
-  })
+  });
+
+  $('#search-close').click(() => {
+    $('#search-screen').hide()
+  });
 })
