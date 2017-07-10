b02168a6aa7b43752fe02bdb2b410911f80ed6ef
Sam Brewer <Sam Brewer>
2017-07-10 12:18:07 -0600

    sort screen doing the thing and search populates when editing

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,26 @@ $(document).ready((e) => {
 
   $('#edit-search').click((e) => {
     $('.search-wrapper').show()
-    console.log($(e.target).closest($('.limit').text()));
+
+    $('#range-limit-value').html($(e.target).parent().parent().parent().prev().find($('.limit')).text())
+    $('#result-limit').val($(e.target).parent().parent().parent().prev().find($('.limit')).text())
+
+    $('#range-volume-value').html($(e.target).parent().parent().parent().prev().find($('.mnvol')).text())
+    $('#min-volume').val($(e.target).parent().parent().parent().prev().find($('.mnvol')).text())
+
+    $('#price-min').val($(e.target).parent().parent().parent().prev().find($('.mnprc')).text())
+
+    $('#price-max').val($(e.target).parent().parent().parent().prev().find($('.mxprc')).text())
+
+    $('#review-min').val($(e.target).parent().parent().parent().prev().find($('.mnrvw')).text())
+
+    $('#review-max').val($(e.target).parent().parent().parent().prev().find($('.mxrvw')).text())
+
+    // $('#sort-by').val($(e.target).parent().parent().parent().prev().find($('.sort-by')).text())
+
+    $('#keywords').val($(e.target).parent().parent().parent().prev().find($('.kywrd')).text())
+
+    console.log($(e.target).parent().parent().parent().prev().find($('.limit')).text())
   });
 
   $('.save-search button').click(() => {
@@ -65,108 +84,11 @@ $(document).ready((e) => {
     $('body').css('overflow-y', 'hidden')
   });
 
-  // $('#avg-revenue').click(() => {
-  //   if ($('#avg-revenue').parent().children().length === 1) {
-  //     $('#avg-revenue').css('color', 'dodgerblue')
-  //
-  //     $('#avg-revenue').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#avg-revenue').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#avg-revenue').parent().children('i').remove()
-  //
-  //     $('#avg-revenue').css('color', 'black')
-  //   }
-  //   console.log($('#avg-revenue').parent().children().length);
-  // })
-  //
-  // $('#num-products').click(() => {
-  //   if ($('#num-products').parent().children().length === 1) {
-  //     $('#num-products').css('color', 'dodgerblue')
-  //
-  //     $('#num-products').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#num-products').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#num-products').parent().children('i').remove()
-  //
-  //     $('#num-products').css('color', 'black')
-  //   }
-  // })
-  //
-  // $('#avg-price').click(() => {
-  //   if ($('#avg-price').parent().children().length === 1) {
-  //     $('#avg-price').css('color', 'dodgerblue')
-  //
-  //     $('#avg-price').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#avg-price').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#avg-price').parent().children('i').remove()
-  //
-  //     $('#avg-price').css('color', 'black')
-  //   }
-  // })
-  //
-  // $('#avg-volume').click(() => {
-  //   if ($('#avg-volume').parent().children().length === 1) {
-  //     $('#avg-volume').css('color', 'dodgerblue')
-  //
-  //     $('#avg-volume').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#avg-volume').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#avg-volume').parent().children('i').remove()
-  //
-  //     $('#avg-volume').css('color', 'black')
-  //   }
-  // })
-  //
-  // $('#page-space').click(() => {
-  //   if ($('#page-space').parent().children().length === 1) {
-  //     $('#page-space').css('color', 'dodgerblue')
-  //
-  //     $('#page-space').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#page-space').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#page-space').parent().children('i').remove()
-  //
-  //     $('#page-space').css('color', 'black')
-  //   }
-  //  })
-  //
-  // $('#avg-review').click(() => {
-  //   if ($('#avg-review').parent().children().length === 1) {
-  //     $('#avg-review').css('color', 'dodgerblue')
-  //
-  //     $('#avg-review').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#avg-review').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#avg-review').parent().children('i').remove()
-  //
-  //     $('#avg-review').css('color', 'black')
-  //   }
-  // })
-  // $('#max-revenue').click(() => {
-  //   if ($('#max-revenue').parent().children().length === 1) {
-  //     $('#max-revenue').css('color', 'dodgerblue')
-  //
-  //     $('#max-revenue').parent().append('<i class="fa fa-check" style="color: dodgerblue;"></i>')
-  //
-  //     $('#max-revenue').parent().css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'})
-  //   } else {
-  //     $('#max-revenue').parent().children('i').remove()
-  //
-  //     $('#max-revenue').css('color', 'black')
-  //   }
-  // })
-
   $('#sort-cancel').click(() => {
     $('.sort-wrapper').hide()
     $('body').css('overflow-y', 'auto')
   });
+
   $('#sort-done').click(() => {
     $('.sort-wrapper').hide()
     $('body').css('overflow-y', 'auto')
